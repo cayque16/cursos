d5314894e2d15b730a5bd529b4e8d70dd50f31d1
Cayque Santos <cayque_santos16@hotmail.com>
2022-12-14 18:41:36 -0300

    feat(desafio_ci): adicionar um index para usar as funcoes
    Foi criado um index para executar todas as funcoes da calculadora

---
+++ full_cycle/ci/desafio01/src/index.js
@@ -0,0 +1,9 @@
+const soma = require('./soma');
+const sub = require('./sub');
+const mult = require('./mult');
+const div = require('./div');
+
+console.log("Uma soma: 5 + 5 = " + soma(5, 5));
+console.log("Uma subtração: 25 - 5 = " + sub(25, 5));
+console.log("Uma divisão: 30 / 6 = " + div(30, 6));
+console.log("Uma multiplicação: 10 * 5 = " + mult(10, 5));
