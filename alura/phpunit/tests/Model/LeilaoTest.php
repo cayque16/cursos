4e296846f4d8d21fb93d266c175feda0bdf30c41
Cayque Santos <cayque_santos16@hotmail.com>
2023-02-25 10:57:12 -0300

    Adicionar o curso de phpunit da Alura.

---
+++ alura/phpunit/tests/Model/LeilaoTest.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Alura\Leilao\Tests\Model;
+
+use Alura\Leilao\Model\Lance;
+use Alura\Leilao\Model\Leilao;
+use Alura\Leilao\Model\Usuario;
+use PHPUnit\Framework\TestCase;
+
+class LeilaoTest extends TestCase
+{
+    public function testLeilaoNaoDeveReceberLancesRepetidos()
+    {
+        $this->expectException(\DomainException::class);
+        $this->expectExceptionMessage('Usuário não pode propor 2 lances consecutivos.');
+        $leilao = new Leilao('Variante');
+        $ana = new Usuario('Ana');
+
+        $leilao->recebeLance(new Lance($ana, 1000));
+        $leilao->recebeLance(new Lance($ana, 1500));
+    }
+
+    public function testLeilaoNaoDeveAceitarMaisDe5LancesPorUsuario()
+    {
+        $this->expectException(\DomainException::class);
+        $this->expectExceptionMessage('Usuário não pode propor mais de 5 lances.');
+
+        $leilao = new Leilao('Brasília Amarela');
+        $joao = new Usuario('João');
+        $maria = new Usuario('Maria');
+
+        $leilao->recebeLance(new Lance($joao, 1000));
+        $leilao->recebeLance(new Lance($maria, 1500));
+        $leilao->recebeLance(new Lance($joao, 1000));
+        $leilao->recebeLance(new Lance($maria, 2500));
+        $leilao->recebeLance(new Lance($joao, 3000));
+        $leilao->recebeLance(new Lance($maria, 3500));
+        $leilao->recebeLance(new Lance($joao, 4000));
+        $leilao->recebeLance(new Lance($maria, 4500));
+        $leilao->recebeLance(new Lance($joao, 5000));
+        $leilao->recebeLance(new Lance($maria, 5500));
+
+        $leilao->recebeLance(new Lance($joao, 6000));
+    }
+
+    /**
+     * @dataProvider geraLances
+     */
+    public function testLeilaoDeveReceberLances(
+        int $qtdeLances,
+        Leilao $leilao,
+        array $valores
+    ) {
+        $this->assertCount($qtdeLances, $leilao->getLances());
+
+        foreach ($valores as $i => $valorEsperado) {
+            $this->assertEquals($valorEsperado, $leilao->getLances()[$i]->getValor());
+        }
+    }
+
+    public static function geraLances()
+    {
+        $joao = new Usuario('João');
+        $maria = new Usuario('Maria');
+
+        $leilaoCom2Lances = new Leilao('Fiat 147 0KM');
+        $leilaoCom2Lances->recebeLance(new Lance($joao, 1000));
+        $leilaoCom2Lances->recebeLance(new Lance($maria, 2000));
+
+        $leilaoCom1Lance = new Leilao('Fusca 1972 0KM');
+        $leilaoCom1Lance->recebeLance(new Lance($joao, 5000));
+
+        return [
+            '2Lances' => [2, $leilaoCom2Lances, [1000, 2000]],
+            '1Lance' => [1, $leilaoCom1Lance, [5000]]
+        ];
+    }
+}
