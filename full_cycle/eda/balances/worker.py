4ce2fb1dacff066d0f8cc39a159ed5f7284070c6
Cayque Santos <cayque_santos16@hotmail.com>
2023-06-21 20:44:39 -0300

    Adicionar worker responsavel por monitorar as transacoes.

---
+++ full_cycle/eda/balances/worker.py
@@ -0,0 +1,33 @@
+import json
+import mysql.connector as mysql
+from kafka import KafkaConsumer
+
+if __name__ == '__main__':
+    conn = mysql.connect(
+        host="192.168.2.10",
+        user="root",
+        password="root",
+        database="balances",
+        port=3306
+    )
+
+    while True:
+        consumer = KafkaConsumer(
+            'transactions',
+            bootstrap_servers='192.168.2.10:9092',
+            auto_offset_reset='earliest'
+        )
+        for msg in consumer:
+            trasaction = json.loads(msg.value)
+
+            amount = trasaction['Payload']['amount']
+
+            sql_reduce = """UPDATE balance 
+                SET balance = balance - {}
+                WHERE account_id = "{}"
+            """.format(amount, trasaction['Payload']['account_id_from'])
+
+            sql_add = """UPDATE balance 
+                SET balance = balance + {}
+                WHERE account_id = "{}"
+            """.format(amount, trasaction['Payload']['account_id_to'])
