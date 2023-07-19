7348fe21cd2a87165784a7425fa8fa0a6fcc879f
Cayque Santos <cayque_santos16@hotmail.com>
2023-07-19 19:13:36 -0300

    Usar prepared staements ao se inserir alunos.

--- alura/PDO/inserir-aluno.php
+++ alura/PDO/inserir-aluno.php
@@ -8,6 +8,11 @@ $pdo = new PDO('sqlite:banco.sqlite');
 
 $student = new Student(null, 'Cayque Santos', new \DateTimeImmutable('1994-04-15'));
 
-$sqlInsert = "INSERT INTO students (name, birth_date) VALUES ('{$student->name()}', '{$student->birthDate()->format('Y-m-d')}');";
+$sqlInsert = "INSERT INTO students (name, birth_date) VALUES (?, ?);";
+$statement = $pdo->prepare($sqlInsert);
+$statement->bindValue(1, $student->name());
+$statement->bindValue(2, $student->birthDate()->format('Y-m-d'));
 
-var_dump($pdo->exec($sqlInsert));
+if ($statement->execute()) {
+    echo "Aluno incluído" . PHP_EOL;
+}
