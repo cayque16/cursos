fbf737a0b23ff64519ce667ba8c84338d691398a
Cayque Santos <cayque_santos16@hotmail.com>
2023-07-20 19:28:30 -0300

    Adicionar importacao da model Student.

--- alura/PDO/src/Infrastructure/Repository/PdoStudentRepository.php
+++ alura/PDO/src/Infrastructure/Repository/PdoStudentRepository.php
@@ -4,6 +4,7 @@ namespace Alura\Pdo\Infrastructure\Repository;
 
 use Alura\Pdo\Domain\Repository\StudentRepository;
 use Alura\Pdo\Infrastructure\Persistence\ConnectionCreator;
+use Alura\Pdo\Domain\Model\Student;
 use PDO;
 
 class PdoStudentRepository implements StudentRepository
