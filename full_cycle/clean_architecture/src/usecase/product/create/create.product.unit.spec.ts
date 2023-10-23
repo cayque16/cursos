3ba4952946005392452039fd8942c8f10522c691
Cayque Santos <cayque_santos16@hotmail.com>
2023-10-23 20:13:03 -0300

    Adicionar testes para os campos names e price.

--- full_cycle/clean_architecture/src/usecase/product/create/create.product.unit.spec.ts
+++ full_cycle/clean_architecture/src/usecase/product/create/create.product.unit.spec.ts
@@ -27,4 +27,27 @@ describe("unit test create product use case", () => {
             price: input.price
         });
     });
+
+    it("should thrown an error when name is missing", async () => {
+        const productRepository = MockRepository();
+        const productCreateUseCase = new CreateProductUseCase(productRepository);
+
+        input.name = "";
+
+        await expect(productCreateUseCase.execute(input)).rejects.toThrow(
+            "Name is required"
+        );
+    });
+
+    it("should throw error when price is less than zero", async () => {
+        const productRepository = MockRepository();
+        const productCreateUseCase = new CreateProductUseCase(productRepository);
+
+        input.name = "Teste";
+        input.price = -1;
+
+        await expect(productCreateUseCase.execute(input)).rejects.toThrow(
+            "Price must be greater than zero"
+        );
+    });
 });
