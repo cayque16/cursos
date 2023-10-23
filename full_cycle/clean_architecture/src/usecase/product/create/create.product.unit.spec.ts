cfde7121da846c5c0ffb351d51070b776aec8fa0
Cayque Santos <cayque_santos16@hotmail.com>
2023-10-23 19:52:54 -0300

    Criar use cases da operacao create para Product.

---
+++ full_cycle/clean_architecture/src/usecase/product/create/create.product.unit.spec.ts
@@ -0,0 +1,30 @@
+import CreateProductUseCase from "./create.product.usecase";
+
+const input = {
+    name: "Fritadeira",
+    price: 900
+}
+
+const MockRepository = () => {
+    return {
+        find: jest.fn(),
+        findAll: jest.fn(),
+        create: jest.fn(),
+        update: jest.fn(),
+    };
+};
+
+describe("unit test create product use case", () => {
+    it("should create a product", async () => {
+        const productRepository = MockRepository();
+        const productCreateUseCase = new CreateProductUseCase(productRepository);
+
+        const output = await productCreateUseCase.execute(input);
+
+        expect(output).toEqual({
+            id: expect.any(String),
+            name: input.name,
+            price: input.price
+        });
+    });
+});
