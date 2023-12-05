ad1d6d0027b10c1b26a405afe89657a5825a8d18
Cayque Santos <cayque_santos16@hotmail.com>
2023-12-04 21:45:14 -0300

    Adicionar teste de integracao do use case update para product.

---
+++ full_cycle/clean_architecture/src/usecase/product/update/update.product.integration.spec.ts
@@ -0,0 +1,46 @@
+import { Sequelize } from "sequelize-typescript";
+import ProductModel from "../../../infrastructure/product/repository/sequelize/product.model";
+import ProductFactory from "../../../domain/product/factory/product.factory";
+import ProductRepository from "../../../infrastructure/product/repository/sequelize/product.repository";
+import UpdateProductUseCase from "./update.product.usecase";
+import Product from "../../../domain/product/entity/product";
+
+const product = new Product('123', 'Fritadeira', 800);
+
+const input = {
+    id: product.id,
+    name: "Ventilador",
+    price: 400
+};
+
+describe("Integration test for update product use case", () => {
+    let sequelize: Sequelize;
+
+    beforeEach(async () => {
+        sequelize = new Sequelize({
+            dialect: "sqlite",
+            storage: ":memory:",
+            logging: false,
+            sync: {force: true}
+        })
+
+        await sequelize.addModels([ProductModel])
+        await sequelize.sync()
+    })
+
+    afterEach(async () => {
+        await sequelize.close();
+    })
+
+    it("should update a product", async () => {
+        const productRepository = new ProductRepository();
+
+        productRepository.create(product)
+
+        const productUpdateUseCase = new UpdateProductUseCase(productRepository);
+    
+        const output = await productUpdateUseCase.execute(input);
+    
+        expect(output).toEqual(input);
+    })
+})
