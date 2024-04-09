65350c50e87f175279ba8d1c279ad9ec0f51661a
Cayque Santos <cayque_santos16@hotmail.com>
2024-04-09 10:19:59 -0300

    Adicionar rota para listar products.

--- full_cycle/clean_architecture/src/infrastructure/api/__tests__/product.e2e.spec.ts
+++ full_cycle/clean_architecture/src/infrastructure/api/__tests__/product.e2e.spec.ts
@@ -29,4 +29,32 @@ describe("E2E test for customer", () => {
         });
         expect(response.status).toBe(500);
     });
+
+    it("should list all product", async () => {
+        const response = await request(app)
+            .post("/product")
+            .send({
+                name: "Fritadeira",
+                price: 800
+            });
+        expect(response.status).toBe(200);
+        const response2 = await request(app)
+            .post("/product")
+            .send({
+                name: "Forno",
+                price: 1000
+            });
+        expect(response2.status).toBe(200);
+
+        const listResponse = await request(app).get("/product").send();
+
+        expect(listResponse.status).toBe(200);
+        expect(listResponse.body.products.length).toBe(2);
+        const product = listResponse.body.products[0];
+        expect(product.name).toBe("Fritadeira");
+        expect(product.price).toBe(800);
+        const product2 = listResponse.body.products[1];
+        expect(product2.name).toBe("Forno");
+        expect(product2.price).toBe(1000);
+    });
 });
--- full_cycle/clean_architecture/src/infrastructure/api/routes/product.route.ts
+++ full_cycle/clean_architecture/src/infrastructure/api/routes/product.route.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from "express";
 import CreateProductUseCase from "../../../usecase/product/create/create.product.usecase";
 import ProductRepository from "../../product/repository/sequelize/product.repository";
+import ListProductUseCase from "../../../usecase/product/list/list.product.usecase";
 
 export const productRoute = express.Router();
 
@@ -13,6 +14,16 @@ productRoute.post("/", async (req: Request, res: Response) => {
         };
 
         const output = await usecase.execute(productDto);
+        res.send(output);
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
+
+productRoute.get("/", async (req: Request, res: Response) => {
+    const usecase = new ListProductUseCase(new ProductRepository());
+    try {
+        const output = await usecase.execute({});
         res.send(output);
     } catch (err) {
         res.status(500).send(err);
