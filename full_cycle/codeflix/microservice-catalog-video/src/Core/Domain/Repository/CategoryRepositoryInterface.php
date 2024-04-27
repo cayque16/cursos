461d0df7b737705dc08edd02a6c63b6f0b4e0e1b
Cayque Santos <cayque_santos16@hotmail.com>
2024-04-27 16:08:39 -0300

    Adicionar CategoryRepositoryInterface.

---
+++ full_cycle/codeflix/microservice-catalog-video/src/Core/Domain/Repository/CategoryRepositoryInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+use Core\Domain\Entity\Category;
+
+interface CategoryRepositoryInterface
+{
+    public function insert(Category $category): Category;
+    public function findById(string $id): Category;
+    public function findAll(string $filter = '', $order = 'DESC'): array;
+    public function paginate(string $filter = '', $order = 'DESC', int $page = 1, $totalPage = 15): array;
+    public function update(Category $category): Category;
+    public function delete(string $id): bool;
+    public function toCategory(object $data): Category;
+}
