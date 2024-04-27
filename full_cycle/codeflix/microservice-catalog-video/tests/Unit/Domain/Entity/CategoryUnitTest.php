5285c399d1380e0f3321d54479e8e994b35523ad
Cayque Santos <cayque_santos16@hotmail.com>
2024-04-26 23:34:10 -0300

    Adicionar classe Category,

---
+++ full_cycle/codeflix/microservice-catalog-video/tests/Unit/Domain/Entity/CategoryUnitTest.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Tests\Unit\Domain\Entity;
+
+use Core\Domain\Entity\Category;
+use PHPUnit\Framework\TestCase;
+
+class CategoryUnitTest extends TestCase
+{
+    public function testAttributes()
+    {
+        $category = new Category(
+            name: 'New cat',
+            description: 'New desc',
+            isActive: true
+        );
+
+        $this->assertEquals('New cat', $category->name);
+        $this->assertEquals('New desc', $category->description);
+        $this->assertEquals(true, $category->isActive);
+    }
+
+    public function testActived()
+    {
+        $category = new Category(
+            name: 'New cat',
+            isActive: false
+        );
+
+        $this->assertFalse($category->isActive);
+        $category->activate();
+        $this->assertTrue($category->isActive);
+    }
+
+    public function testDisabled()
+    {
+        $category = new Category(
+            name: 'New cat',
+        );
+
+        $this->assertTrue($category->isActive);
+        $category->disable();
+        $this->assertFalse($category->isActive);
+    }
+
+    public function testUpdate()
+    {
+        $uuid = 'uuid.value';
+
+        $category = new Category(
+            id: $uuid,
+            name: 'New cat',
+            description: 'New desc',
+            isActive: false
+        );
+
+        $category->update(
+            name: 'new_name',
+            description: 'new_desc',
+        );
+
+        $this->assertEquals('new_name', $category->name);
+        $this->assertEquals('new_desc', $category->description);
+    }
+}
