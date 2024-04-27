7e961f589c4f1685e062336d10e6acd7c2e94c70
Cayque Santos <cayque_santos16@hotmail.com>
2024-04-27 15:56:36 -0300

    Adicionar o uso do uuid.

--- full_cycle/codeflix/microservice-catalog-video/src/Core/Domain/Entity/Traits/MethodsMagicsTrait.php
+++ full_cycle/codeflix/microservice-catalog-video/src/Core/Domain/Entity/Traits/MethodsMagicsTrait.php
@@ -15,4 +15,9 @@ trait MethodsMagicsTrait
         $className = get_class($this);
         throw new Exception("Property {$property} not found in {$className}");
     }
+
+    public function id(): string
+    {
+        return (string) $this->id;
+    }
 }
--- full_cycle/codeflix/microservice-catalog-video/tests/Unit/Domain/Entity/CategoryUnitTest.php
+++ full_cycle/codeflix/microservice-catalog-video/tests/Unit/Domain/Entity/CategoryUnitTest.php
@@ -5,6 +5,7 @@ namespace Tests\Unit\Domain\Entity;
 use Core\Domain\Entity\Category;
 use Core\Domain\Exception\EntityValidationException;
 use PHPUnit\Framework\TestCase;
+use Ramsey\Uuid\Uuid;
 use Throwable;
 
 class CategoryUnitTest extends TestCase
@@ -17,6 +18,7 @@ class CategoryUnitTest extends TestCase
             isActive: true
         );
 
+        $this->assertNotEmpty($category->id());
         $this->assertEquals('New cat', $category->name);
         $this->assertEquals('New desc', $category->description);
         $this->assertEquals(true, $category->isActive);
@@ -47,7 +49,7 @@ class CategoryUnitTest extends TestCase
 
     public function testUpdate()
     {
-        $uuid = 'uuid.value';
+        $uuid = (string) Uuid::uuid4()->toString();
 
         $category = new Category(
             id: $uuid,
@@ -61,6 +63,7 @@ class CategoryUnitTest extends TestCase
             description: 'new_desc',
         );
 
+        $this->assertEquals($uuid, $category->id());
         $this->assertEquals('new_name', $category->name);
         $this->assertEquals('new_desc', $category->description);
     }
