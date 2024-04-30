483e84630ea2fef10d88a7170ac541c50f0f077a
Cayque Santos <cayque_santos16@hotmail.com>
2024-04-30 13:50:11 -0300

    Adicionar a interface PaginationInterface.

---
+++ full_cycle/codeflix/microservice-catalog-video/src/Core/Domain/Repository/PaginationInterface.php
@@ -0,0 +1,18 @@
+<?php
+
+use Core\Domain\Entity\Category;
+
+interface PaginationInterface
+{
+    /**
+     * @return stdClass[]
+     */
+    public function items(): array;
+    public function total(): int;
+    public function lastPage(): int;
+    public function firstPage(): int;
+    public function currentPage(): int;
+    public function perPage(): int;
+    public function to(): int;
+    public function from(): int;
+}
