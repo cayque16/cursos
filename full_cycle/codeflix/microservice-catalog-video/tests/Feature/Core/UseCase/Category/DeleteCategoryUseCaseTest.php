f5fc81bc1b2834fe1b689c17988867bc00ed8c23
Cayque Santos <cayque_santos16@hotmail.com>
2024-05-02 14:25:17 -0300

    Adicionar teste de intergacao do usecase delete.

---
+++ full_cycle/codeflix/microservice-catalog-video/tests/Feature/Core/UseCase/Category/DeleteCategoryUseCaseTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Feature\Core\UseCase\Category;
+
+use Tests\TestCase;
+use App\Models\Category as CategoryModel;
+use App\Repositories\Eloquent\CategoryEloquentRepository;
+use Core\UseCase\Category\DeleteCategoryUseCase;
+use Core\UseCase\DTO\Category\CategoryInputDto;
+
+class DeleteCategoryUseCaseTest extends TestCase
+{
+    public function testDelete()
+    {
+        $category = CategoryModel::factory()->create();
+
+        $repository = new CategoryEloquentRepository(new CategoryModel());
+        $useCase = new DeleteCategoryUseCase($repository);
+
+        $useCase->execute(
+            new CategoryInputDto(id: $category->id)
+        );
+
+        $this->assertSoftDeleted($category);
+    }
+}
