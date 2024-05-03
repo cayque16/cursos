0ee09cddb2d03b1d27b1ec5207a1c0eb2c92a674
Cayque Santos <cayque_santos16@hotmail.com>
2024-05-03 20:37:38 -0300

    Implementar o insert do GenreEloquentRepository.

---
+++ full_cycle/codeflix/microservice-catalog-video/app/Repositories/Eloquent/GenreEloquentRepository.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Repositories\Eloquent;
+
+use Core\Domain\Entity\Genre as GenreEntity;
+use App\Models\Genre as GenreModel;
+use Core\Domain\Repository\GenreRepositoryInterface;
+use Core\Domain\Repository\PaginationInterface;
+use Core\Domain\ValueObject\Uuid;
+use DateTime;
+
+class GenreEloquentRepository implements GenreRepositoryInterface
+{
+    public function __construct(
+        protected GenreModel $model
+    ) { }
+
+    public function insert(GenreEntity $genre): GenreEntity
+    {
+        $genreDb = $this->model->create([
+            'id' => $genre->id(),
+            'name' => $genre->name,
+            'is_active' => $genre->isActive,
+            'created_at' => $genre->createdAt(),
+        ]);
+
+        if (count($genre->categories) > 0) {
+            $genreDb->categories()->sync($genre->categories);
+        }
+
+        return $this->toGenre($genreDb);
+    }
+
+    public function findById(string $id): GenreEntity
+    {
+
+    }
+
+    public function findAll(string $filter = '', $order = 'DESC'): array
+    {
+
+    }
+
+    public function paginate(string $filter = '', $order = 'DESC', int $page = 1, $totalPage = 15): PaginationInterface
+    {
+
+    }
+
+    public function update(GenreEntity $genre): GenreEntity
+    {
+
+    }
+
+    public function delete(string $id): bool
+    {
+
+    }
+    public function toGenre(object $data): GenreEntity
+    {
+        $entity = new GenreEntity(
+            id: new Uuid($data->id),
+            name: $data->name,
+            createdAt: new DateTime($data->created_at),
+        );
+
+        ((bool) $data->is_active) ? $entity->activate() : $entity->disable();
+
+        return $entity;
+    }
+}
---
+++ full_cycle/codeflix/microservice-catalog-video/tests/Feature/App/Repositories/Eloquent/GenreEloquentRepositoryTest.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Tests\Feature\App\Repositories\Eloquent;
+
+use App\Models\Category;
+use App\Models\Genre as GenreModel;
+use App\Repositories\Eloquent\GenreEloquentRepository;
+use Core\Domain\Entity\Genre as GenreEntity;
+use Core\Domain\Repository\GenreRepositoryInterface;
+use Tests\TestCase;
+
+class GenreEloquentRepositoryTest extends TestCase
+{
+    protected $repository;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->repository = new GenreEloquentRepository(new GenreModel());
+    }
+
+    public function testImplementsInterface()
+    {
+        $this->assertInstanceOf(GenreRepositoryInterface::class, $this->repository);
+    }
+
+    public function testInsert()
+    {
+        $entity = new GenreEntity(name: 'New genre');
+
+        $response = $this->repository->insert($entity);
+
+        $this->assertEquals($entity->id, $response->id);
+        $this->assertEquals($entity->name, $response->name);
+        $this->assertDatabaseHas('genres', [
+            'id' => $entity->id()
+        ]);
+    }
+
+    public function testInsertDeactivate()
+    {
+        $entity = new GenreEntity(name: 'New genre');
+        $entity->disable();
+
+        $response = $this->repository->insert($entity);
+
+        $this->assertDatabaseHas('genres', [
+            'id' => $entity->id(),
+            'is_active' => false,
+        ]);
+    }
+
+    public function testInsertWithRelationships()
+    {
+        $categories = Category::factory()->count(4)->create();
+
+        $genre = new GenreEntity(name: 'teste');
+        foreach ($categories as $category) {
+            $genre->addCategory($category->id);
+        }
+
+        $response = $this->repository->insert($genre);
+
+        $this->assertDatabaseHas('genres', [
+            'id' => $response->id(),
+        ]);
+
+        $this->assertDatabaseCount('category_genre', 4);
+    }
+}
