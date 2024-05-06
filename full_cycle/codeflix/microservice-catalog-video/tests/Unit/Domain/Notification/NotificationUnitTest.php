39d689e50c6db49c8bd3fdabfdd8de4e76e26a7f
Cayque Santos <cayque_santos16@hotmail.com>
2024-05-06 15:52:40 -0300

    Incluir classe Notification.

---
+++ full_cycle/codeflix/microservice-catalog-video/tests/Unit/Domain/Notification/NotificationUnitTest.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace Tests\Unit\Domain\Notification;
+
+use Core\Domain\Notification\Notification;
+use PHPUnit\Framework\TestCase;
+
+class NotificationUnitTest extends TestCase
+{
+    public function testGetErrors()
+    {
+        $notification = new Notification();
+
+        $errors = $notification->getErrors();
+
+        $this->assertIsArray($errors);
+    }
+
+    public function testAddErrors()
+    {
+        $notification = new Notification();
+        
+        $notification->addError([
+            'context' => 'video',
+            'message' => 'video title is required',
+        ]);
+
+        $this->assertCount(1, $notification->getErrors());
+    }
+
+    public function testHasErrors()
+    {
+        $notification = new Notification();
+
+        $this->assertFalse($notification->hasErrors());
+
+        $notification->addError([
+            'context' => 'video',
+            'message' => 'video title is required',
+        ]);
+
+        $this->assertTrue($notification->hasErrors());
+    }
+
+    public function testMessage()
+    {
+        $notification = new Notification();
+        $notification->addError([
+            'context' => 'video',
+            'message' => 'title is required',
+        ]);
+        $notification->addError([
+            'context' => 'video',
+            'message' => 'description is required',
+        ]);
+
+        $message = $notification->messages();
+
+        $this->assertIsString($message);
+        $this->assertEquals(
+            expected: 'video: title is required,video: description is required,',
+            actual: $message
+        );
+    }
+
+    public function testMessageFilterContext()
+    {
+        $notification = new Notification();
+        $notification->addError([
+            'context' => 'video',
+            'message' => 'title is required',
+        ]);
+        $notification->addError([
+            'context' => 'category',
+            'message' => 'description is required',
+        ]);
+
+        $this->assertCount(2, $notification->getErrors());
+
+        $message = $notification->messages('video');
+
+        $this->assertIsString($message);
+        $this->assertEquals(
+            expected: 'video: title is required,',
+            actual: $message
+        );
+    }
+}
