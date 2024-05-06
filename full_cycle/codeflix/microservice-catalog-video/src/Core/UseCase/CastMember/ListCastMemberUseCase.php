e19dc8923942be1f744895cc7d563b4e67ffefc3
Cayque Santos <cayque_santos16@hotmail.com>
2024-05-05 23:59:49 -0300

    Implementar o ListCastMemberUseCase.

---
+++ full_cycle/codeflix/microservice-catalog-video/src/Core/UseCase/CastMember/ListCastMemberUseCase.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Core\UseCase\CastMember;
+
+use Core\Domain\Repository\CastMemberRepositoryInterface;
+use Core\UseCase\DTO\CastMember\CastMemberInputDto;
+use Core\UseCase\DTO\CastMember\CastMemberOutputDto;
+
+class ListCastMemberUseCase
+{
+    public function __construct(
+        protected CastMemberRepositoryInterface $repository,
+    ) { }
+
+    public function execute(CastMemberInputDto $input): CastMemberOutputDto
+    {
+        $castMember = $this->repository->findById($input->id);
+
+        return new CastMemberOutputDto(
+            id: $castMember->id(),
+            name: $castMember->name,
+            type: $castMember->type->value,
+            createdAt: $castMember->createdAt()
+        );
+    }
+}
---
+++ full_cycle/codeflix/microservice-catalog-video/tests/Unit/UseCase/CastMember/ListCastMemberCaseUnitTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Tests\Unit\UseCase\CastMember;
+
+use PHPUnit\Framework\TestCase;
+use Ramsey\Uuid\Uuid as RamseyUuid;
+use Core\Domain\Entity\CastMember as CastMemberEntity;
+use Core\Domain\Enum\CastMemberType;
+use Core\Domain\Repository\CastMemberRepositoryInterface;
+use Core\UseCase\CastMember\ListCastMemberUseCase;
+use Core\UseCase\DTO\CastMember\CastMemberInputDto;
+use Core\UseCase\DTO\CastMember\CastMemberOutputDto;
+use Mockery;
+use stdClass;
+
+class ListCastMemberCaseUnitTest extends TestCase
+{
+    public function testList()
+    {
+        $uuid = (string) RamseyUuid::uuid4();
+
+        $mockEntity = Mockery::mock(CastMemberEntity::class, ['name', CastMemberType::ACTOR]);
+        $mockEntity->shouldReceive('id')->andReturn($uuid);
+        $mockEntity->shouldReceive('createdAt')->andReturn(date('Y-m-d H:i:s'));
+
+        $mockRepository = Mockery::mock(
+            stdClass::class,
+            CastMemberRepositoryInterface::class
+        );
+        $mockRepository->shouldReceive('findById')
+                        ->times(1)
+                        ->with($uuid)
+                        ->andReturn($mockEntity);
+
+        $mockInputDto = Mockery::mock(CastMemberInputDto::class, [$uuid]);
+
+        $useCase = new ListCastMemberUseCase($mockRepository);
+        
+        $response = $useCase->execute($mockInputDto);
+
+        $this->assertInstanceOf(CastMemberOutputDto::class, $response);
+
+        Mockery::close();
+    }
+}
