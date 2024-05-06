bb1032dce13eb76f9729f2eadcceb3983012a70d
Cayque Santos <cayque_santos16@hotmail.com>
2024-05-06 08:18:32 -0300

    Implementar o DeleteCastMemberUseCase.

---
+++ full_cycle/codeflix/microservice-catalog-video/src/Core/UseCase/CastMember/DeleteCastMemberUseCase.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Core\UseCase\CastMember;
+
+use Core\Domain\Repository\CastMemberRepositoryInterface;
+use Core\UseCase\DTO\CastMember\CastMemberInputDto;
+use Core\UseCase\DTO\CastMember\DeleteCastMember\DeleteCastMemberOutputDto;
+
+class DeleteCastMemberUseCase
+{
+    public function __construct(
+        protected CastMemberRepositoryInterface $repository,
+    ) { }
+
+    public function execute(CastMemberInputDto $input): DeleteCastMemberOutputDto
+    {
+        $hasDeleted = $this->repository->delete($input->id);
+
+        return new DeleteCastMemberOutputDto($hasDeleted);
+    }
+}
---
+++ full_cycle/codeflix/microservice-catalog-video/tests/Unit/UseCase/CastMember/DeleteCastMemberUseCaseUnitTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tests\Unit\UseCase\CastMember;
+
+use Core\Domain\Repository\CastMemberRepositoryInterface;
+use Core\UseCase\CastMember\DeleteCastMemberUseCase;
+use Core\UseCase\DTO\CastMember\CastMemberInputDto;
+use Core\UseCase\DTO\CastMember\DeleteCastMember\DeleteCastMemberOutputDto;
+use Mockery;
+use PHPUnit\Framework\TestCase;
+use Ramsey\Uuid\Uuid;
+use stdClass;
+
+class DeleteCastMemberUseCaseUnitTest extends TestCase
+{
+    public function testDelete()
+    {
+        $uuid = (string) Uuid::uuid4();
+
+        $mockRepository = Mockery::mock(
+            stdClass::class,
+            CastMemberRepositoryInterface::class
+        );
+        $mockRepository->shouldReceive('delete')
+                        ->once()
+                        ->andReturn(true);
+
+        $mockInputDto = Mockery::mock(CastMemberInputDto::class, [$uuid]);
+
+        $useCase = new DeleteCastMemberUseCase($mockRepository);
+        
+        $response = $useCase->execute($mockInputDto);
+
+        $this->assertInstanceOf(DeleteCastMemberOutputDto::class, $response);
+    }
+}
