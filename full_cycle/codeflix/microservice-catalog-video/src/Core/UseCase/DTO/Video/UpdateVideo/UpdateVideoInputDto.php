0299f26ad700e661375ab09b1f74d0b0dd15c64c
Cayque Santos <cayque_santos16@hotmail.com>
2024-05-07 15:59:46 -0300

    Adicionar os UpdateVideoInputDto e UpdateVideoOutputDto.

---
+++ full_cycle/codeflix/microservice-catalog-video/src/Core/UseCase/DTO/Video/UpdateVideo/UpdateVideoInputDto.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Core\UseCase\DTO\Video\UpdateVideo;
+
+class UpdateVideoInputDto
+{
+    public function __construct(
+        public string $id,
+        public string $title,
+        public string $description,
+        public array $categories,
+        public array $genres,
+        public array $castMembers,
+        public ?array $thumbFile = null,
+        public ?array $thumbHalfFile = null,
+        public ?array $bannerFile = null,
+        public ?array $trailerFile = null,
+        public ?array $videoFile = null,
+    ) { }
+}
