acd9cc6b49a968039cdf820aa0b71cb7dffd2b4b
Cayque Santos <cayque_santos16@hotmail.com>
2024-05-10 22:12:02 -0300

    Criar os relacionamentos entre as tabelas nos models.

--- full_cycle/codeflix/microservice-catalog-video/app/Models/Video.php
+++ full_cycle/codeflix/microservice-catalog-video/app/Models/Video.php
@@ -2,10 +2,57 @@
 
 namespace App\Models;
 
+use App\Enums\ImageTypes;
+use App\Enums\MediaTypes;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Video extends Model
 {
     use HasFactory;
+
+    public function categories()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function genres()
+    {
+        return $this->belongsTo(Genre::class);
+    }
+
+    public function castMembers()
+    {
+        return $this->belongsTo(CastMember::class, 'cast_member_video');
+    }
+
+    public function media()
+    {
+        return $this->hasOne(Media::class)
+                        ->where('type', MediaTypes::VIDEO->value);
+    }
+
+    public function trailer()
+    {
+        return $this->hasOne(Media::class)
+                        ->where('type', MediaTypes::TRAILER->value);
+    }
+
+    public function banner()
+    {
+        return $this->hasOne(ImagesVideo::class)
+                        ->where('type', ImageTypes::BANNER->value);
+    }
+
+    public function thumb()
+    {
+        return $this->hasOne(ImagesVideo::class)
+                        ->where('type', ImageTypes::THUMB->value);
+    }
+
+    public function thumbHalf()
+    {
+        return $this->hasOne(ImagesVideo::class)
+                        ->where('type', ImageTypes::THUMB_HALF->value);
+    }
 }
