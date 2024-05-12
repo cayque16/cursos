245bef834972e9b04e243ef9c89cd198f90fdf87
Cayque Santos <cayque_santos16@hotmail.com>
2024-05-12 17:32:16 -0300

    Refatorar o MediaModel.

--- full_cycle/codeflix/microservice-catalog-video/app/Models/Media.php
+++ full_cycle/codeflix/microservice-catalog-video/app/Models/Media.php
@@ -2,14 +2,30 @@
 
 namespace App\Models;
 
+use App\Models\Traits\UuidTrait;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Media extends Model
 {
-    use HasFactory;
+    use HasFactory, UuidTrait;
 
     protected $table = 'medias_video';
+
+    protected $fillable = [
+        'file_path',
+        'encode_path',
+        'media_status',
+        'type',
+    ];
+
+    public $incrementing = false;
+
+    protected $casts = [
+        'id' => 'string',
+        'is_active' => 'boolean',
+        'deleted_at' => 'datetime'
+    ];
 
     public function video()
     {
