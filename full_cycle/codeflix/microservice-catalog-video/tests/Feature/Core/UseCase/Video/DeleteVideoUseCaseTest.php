cbb7720f4deeac30d16bd12cdd82dd6df8290727
Cayque Santos <cayque_santos16@hotmail.com>
2024-05-14 17:16:11 -0300

    Adicionar o DeleteVideoUseCaseTest.

---
+++ full_cycle/codeflix/microservice-catalog-video/tests/Feature/Core/UseCase/Video/DeleteVideoUseCaseTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Tests\Feature\Core\UseCase\Video;
+
+use App\Models\Video;
+use Core\Domain\Exception\NotFoundException;
+use Core\Domain\Repository\VideoRepositoryInterface;
+use Core\UseCase\DTO\Video\DeleteVideo\DeleteVideoInputDto;
+use Core\UseCase\Video\DeleteVideoUseCase;
+use Tests\TestCase;
+
+class DeleteVideoUseCaseTest extends TestCase
+{
+    public function testDelete()
+    {
+        $useCase = new DeleteVideoUseCase(
+            $this->app->make(VideoRepositoryInterface::class)
+        );
+
+        $video = Video::factory()->create();
+
+        $response = $useCase->execute(new DeleteVideoInputDto($video->id));
+
+        $this->assertTrue($response->success);
+    }
+
+    public function testDeleteIdNotFound()
+    {
+        $this->expectException(NotFoundException::class);
+        
+        $useCase = new DeleteVideoUseCase(
+            $this->app->make(VideoRepositoryInterface::class)
+        );
+
+        $useCase->execute(new DeleteVideoInputDto('fake_id'));
+    }
+}
