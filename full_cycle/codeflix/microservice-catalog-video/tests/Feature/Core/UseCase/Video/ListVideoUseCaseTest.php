52187e1f95103004f83024c097db50a77d61c546
Cayque Santos <cayque_santos16@hotmail.com>
2024-05-14 17:30:38 -0300

    Adicionar o ListVideoUseCaseTest.

---
+++ full_cycle/codeflix/microservice-catalog-video/tests/Feature/Core/UseCase/Video/ListVideoUseCaseTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Tests\Feature\Core\UseCase\Video;
+
+use App\Models\Video;
+use Core\Domain\Exception\NotFoundException;
+use Core\Domain\Repository\VideoRepositoryInterface;
+use Core\UseCase\DTO\Video\ListVideoInputDto;
+use Core\UseCase\Video\ListVideoUseCase;
+use Tests\TestCase;
+
+class ListVideoUseCaseTest extends TestCase
+{
+    public function testList()
+    {
+        $video = Video::factory()->create();
+        
+        $useCase = new ListVideoUseCase(
+            $this->app->make(VideoRepositoryInterface::class)
+        );
+
+        $response = $useCase->execute(new ListVideoInputDto($video->id));
+
+        $this->assertNotNull($response);
+        $this->assertEquals($video->id, $response->id);
+    }
+
+    public function testNotFound()
+    {
+        $this->expectException(NotFoundException::class);
+
+        $useCase = new ListVideoUseCase(
+            $this->app->make(VideoRepositoryInterface::class)
+        );
+
+        $useCase->execute(new ListVideoInputDto('fake_id'));
+    }
+}
