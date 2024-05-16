fff51a481d90deac98550c37bf4f117ff3b2866c
Cayque Santos <cayque_santos16@hotmail.com>
2024-05-16 20:46:09 -0300

    Adicionar cast nos enums do model de Video.

--- full_cycle/codeflix/microservice-catalog-video/app/Models/Video.php
+++ full_cycle/codeflix/microservice-catalog-video/app/Models/Video.php
@@ -49,30 +49,30 @@ class Video extends Model
     public function media()
     {
         return $this->hasOne(Media::class)
-                        ->where('type', MediaTypes::VIDEO->value);
+                        ->where('type', (string) MediaTypes::VIDEO->value);
     }
 
     public function trailer()
     {
         return $this->hasOne(Media::class)
-                        ->where('type', MediaTypes::TRAILER->value);
+                        ->where('type', (string) MediaTypes::TRAILER->value);
     }
 
     public function banner()
     {
         return $this->hasOne(ImagesVideo::class)
-                        ->where('type', ImageTypes::BANNER->value);
+                        ->where('type', (string) ImageTypes::BANNER->value);
     }
 
     public function thumb()
     {
         return $this->hasOne(ImagesVideo::class)
-                        ->where('type', ImageTypes::THUMB->value);
+                        ->where('type', (string) ImageTypes::THUMB->value);
     }
 
     public function thumbHalf()
     {
         return $this->hasOne(ImagesVideo::class)
-                        ->where('type', ImageTypes::THUMB_HALF->value);
+                        ->where('type', (string) ImageTypes::THUMB_HALF->value);
     }
 }
