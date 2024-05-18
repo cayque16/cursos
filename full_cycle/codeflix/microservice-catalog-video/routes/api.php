30bae4dce0c795c23599750e766c26bedcee61ed
Cayque Santos <cayque_santos16@hotmail.com>
2024-05-18 18:00:58 -0300

    Definir guards de autenticacao de api no Laravel.

--- full_cycle/codeflix/microservice-catalog-video/routes/api.php
+++ full_cycle/codeflix/microservice-catalog-video/routes/api.php
@@ -8,6 +8,12 @@ use App\Http\Controllers\Api\{
 };
 use Illuminate\Support\Facades\Route;
 
+Route::middleware(['auth:api'])->group(function () {
+    Route::get('/me', function() {
+        return true;
+    });
+});
+
 Route::apiResource('/categories', CategoryController::class);
 Route::apiResource('/genres', GenreController::class);
 Route::apiResource('/cast_members', CastMemberController::class);
