595e7785d691208af9e3a544e57b0704ae0dbcf4
Cayque Santos <cayque_santos16@hotmail.com>
2024-05-21 17:59:42 -0300

    Adicionar o slice incial de categorias.

---
+++ full_cycle/codeflix/catalog-video-front-end/src/features/categories/categorySlice.ts
@@ -0,0 +1,40 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+interface Category {
+    id: string;
+    name: string;
+    is_active: boolean;
+    created_at: string;
+    updated_at: string;
+    deleted_at: null | string;
+    description: null | string;
+}
+
+const category: Category = {
+    id: "1234-1234-1234",
+    name: "Test",
+    description: "Test",
+    is_active: true,
+    deleted_at: null,
+    created_at: "2020-01-01",
+    updated_at: "2020-01-01",
+}
+
+export const initialState = [
+    category,
+    { ...category, id: "1234-1234-1234", name: "Test"},
+    { ...category, id: "1234-1234-1234", name: "Test"},
+    { ...category, id: "1234-1234-1234", name: "Test"},
+];
+
+const categoriesSlice = createSlice({
+    name: "categories",
+    initialState: initialState,
+    reducers: {
+        createCategory(state, action) {},
+        updateCategory(state, action) {},
+        deleteCategory(state, action) {},
+    },
+});
+
+export default categoriesSlice.reducer;
