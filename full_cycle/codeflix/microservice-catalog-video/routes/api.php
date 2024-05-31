50120a1997156b1b09372ccc7eb688d99652ac06
Cayque Santos <cayque_santos16@hotmail.com>
2024-05-31 16:43:32 -0300

    Desabilitar a autenticacao

--- full_cycle/codeflix/microservice-catalog-video/routes/api.php
+++ full_cycle/codeflix/microservice-catalog-video/routes/api.php
@@ -6,12 +6,12 @@ use App\Http\Controllers\Api\GenreController;
 use App\Http\Controllers\Api\VideoController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['auth:api', 'can:admin-catalog'])->group(function () {
+// Route::middleware(['auth:api', 'can:admin-catalog'])->group(function () {
     Route::apiResource('/categories', CategoryController::class);
     Route::apiResource('/genres', GenreController::class);
     Route::apiResource('/cast_members', CastMemberController::class);
     Route::apiResource('/videos', VideoController::class);
-});
+// });
 
 Route::get('/', function () {
     return response()->json(['message' => 'success']);
