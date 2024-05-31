68f56cecc0a9ceceb70abdf471383eff17afbf67
Cayque Santos <cayque_santos16@hotmail.com>
2024-05-31 08:38:10 -0300

    Aumentar cobertura de testes do ListCategory.

--- full_cycle/codeflix/catalog-video-front-end/src/features/api/apiSlice.tsx
+++ full_cycle/codeflix/catalog-video-front-end/src/features/api/apiSlice.tsx
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const baseUrl = "http://192.168.2.10:8080/api";
+export const baseUrl = "http://192.168.2.10:8080/api";
 
 export const apiSlice = createApi({
     reducerPath: "api",
