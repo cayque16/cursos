51ff335b9a072d47257c031a7debbfd6954a131d
Cayque Santos <cayque_santos16@hotmail.com>
2024-05-31 09:03:17 -0300

    Aumentar cobertura de testes do ListCategory.

--- full_cycle/codeflix/catalog-video-front-end/src/features/categories/ListCategory.test.tsx
+++ full_cycle/codeflix/catalog-video-front-end/src/features/categories/ListCategory.test.tsx
@@ -1,10 +1,10 @@
 import { rest } from "msw";
 import { setupServer } from "msw/node";
 
-import { renderWithProviders, screen } from "../../utils/test-utils";
+import { renderWithProviders, screen, waitFor } from "../../utils/test-utils";
 import { CategoryList } from "./ListCategory";
 import { baseUrl } from "../api/apiSlice";
-import { categoryResponse } from "../castMembers/mocks";
+import { categoryResponse } from "./mocks";
 
 export const handlers = [
     rest.get(`${baseUrl}/categories`, (_, res, ctx) => {
@@ -28,5 +28,29 @@ describe("ListCategory", () => {
         renderWithProviders(<CategoryList />);
         const loading = screen.getByRole("progressbar");
         expect(loading).toBeInTheDocument();
+    });
+
+    it("should render success state", async () => {
+        renderWithProviders(<CategoryList/>);
+
+        await waitFor(() => {
+            const name = screen.getByText("Experimental");
+            expect(name).toBeInTheDocument();
+        });
+    });
+
+    it("should render error state", async () => {
+        server.use(
+            rest.get(`${baseUrl}/categories`, (_, res, ctx) => {
+                return res(ctx.status(500));
+            })
+        );
+
+        renderWithProviders(<CategoryList />);
+
+        await waitFor(() => {
+            const error = screen.getByText("Error fetching categories");
+            expect(error).toBeInTheDocument();
+        });
     });
 });
