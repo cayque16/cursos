f5cb7c2421bc797b8004be4ff4407503601f9ff2
Cayque Santos <cayque_santos16@hotmail.com>
2024-06-01 14:34:16 -0300

    Remover a clausula unique da category_id

--- full_cycle/codeflix/microservice-catalog-video/database/migrations/2024_05_03_212757_create_category_genre_table.php
+++ full_cycle/codeflix/microservice-catalog-video/database/migrations/2024_05_03_212757_create_category_genre_table.php
@@ -22,8 +22,6 @@ return new class extends Migration
             $table->foreign('genre_id')
                 ->references('id')
                 ->on('genres');
-
-            $table->unique('category_id');
         });
     }
 
