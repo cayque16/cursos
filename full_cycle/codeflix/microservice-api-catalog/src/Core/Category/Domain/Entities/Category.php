d099e4a15ebfb0e0cc435652d098bca38be8ccda
Cayque Santos <cayquesantos@bioextratus.rede.bioextratus>
2024-07-23 22:36:49 -0300

    Adicionar entidade Category.

---
+++ full_cycle/codeflix/microservice-api-catalog/src/Core/Category/Domain/Entities/Category.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Core\Category\Domain\Entities;
+
+use DateTime;
+
+class Category
+{
+    public function __construct(
+        protected string $name,
+        protected ?string $description = null,
+        protected bool $isActive = true,
+        protected ?DateTime $createdAt = null
+    ) {
+        $this->createdAt = $this->createdAt ?? new DateTime();
+    }
+
+    public function __get($name)
+    {
+        return $this->{$name};
+    }
+
+    public function createdAt(): string
+    {
+        return (string) $this->createdAt->format('Y-m-d H:i:s');
+    }
+}
---
+++ full_cycle/codeflix/microservice-api-catalog/tests/Unit/Core/Category/Domain/Entities/CategoryUnitTest.php
@@ -0,0 +1,34 @@
+<?php
+
+use DateTime;
+use Core\Category\Domain\Entities\Category;
+
+beforeEach(fn () => $this->category = new Category(
+    name: 'test',
+));
+
+test("constructor of category and getters and setters", function () {
+    expect($this->category->name)->not->toBeNull();
+    expect($this->category->name)->toBeString();
+    expect($this->category->name)->toBe('test');
+    expect($this->category->description)->toBeNull();
+    expect($this->category->isActive)->toBeTrue();
+    expect($this->category->createdAt)->not->toBeNull();
+    expect($this->category->createdAt)->toBeInstanceOf(DateTime::class);
+
+    $date = new DateTime();
+    $category = new Category(
+        name: 'test',
+        description: 'desc',
+        isActive: false,
+        createdAt: $date,
+    );
+    expect($category->description)->not->toBeNull();
+    expect($category->description)->toBe('desc');
+    expect($category->isActive)->toBeFalsy();
+    expect($category->isActive)->toBeBool();
+    expect($category->createdAt)->not->toBeNull();
+    expect($category->createdAt)->toBe($date);
+    expect($category->createdAt())->toBeString();
+    expect($category->createdAt())->not->toBeNull();
+});
