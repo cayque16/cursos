911b99a99ba38a34c17a0758b2cf58d5b739f224
Cayque Santos <cayquesantos@bioextratus.rede.bioextratus>
2024-07-27 10:53:22 -0300

    Aumentar testes do uuid.

--- full_cycle/codeflix/microservice-api-catalog/tests/Unit/Core/SeedWork/Domain/ValueObjects/UuidUnitTest.php
+++ full_cycle/codeflix/microservice-api-catalog/tests/Unit/Core/SeedWork/Domain/ValueObjects/UuidUnitTest.php
@@ -1,11 +1,21 @@
 <?php
 
 use Core\SeedWork\Domain\ValueObjects\Uuid;
+use Ramsey\Uuid\Uuid as RamseyUuid;
 
 test("should generate a uuid", function () {
     $uuid = (string) Uuid::random();
     expect($uuid)->not->toBeNull();
     expect($uuid)->toBeString();
+});
+
+test('should valid uuid', function (){
+    $uuid = Uuid::random();
+    expect(RamseyUuid::isValid($uuid))->toBeTrue();
+});
+
+test('should instance of uuid', function () {
+    expect(Uuid::random())->toBeInstanceOf(Uuid::class);
 });
 
 test("should throw an exception with an invalid uuid", function () {
