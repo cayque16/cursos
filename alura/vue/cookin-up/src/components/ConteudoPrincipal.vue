40a5ab537e42992f55e88544490e127054197de5
Cayque Santos <cayque_santos16@hotmail.com>
2025-08-18 22:29:22 -0300

    feature: adicionar o componente SelecionarIngredientes.

--- alura/vue/cookin-up/src/components/ConteudoPrincipal.vue
+++ alura/vue/cookin-up/src/components/ConteudoPrincipal.vue
@@ -1,10 +1,13 @@
 <script lang="ts">
+import SelecionarIngredientes from "./SelecionarIngredientes.vue";
+
 export default {
   data() {
     return {
       ingredientes: ["Alho", "Manteiga", "Orégano"],
     };
   },
+  components: { SelecionarIngredientes },
 };
 </script>
 
@@ -29,6 +32,7 @@ export default {
         Sua lista está vazia, selecione ingredientes para iniciar.
       </p>
     </section>
+    <SelecionarIngredientes />
   </main>
 </template>
 
