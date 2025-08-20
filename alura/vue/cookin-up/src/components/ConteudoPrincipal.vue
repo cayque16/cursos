661133b64d6121046eb65100a4ad8ede7c4410f5
Cayque Santos <cayque_santos16@hotmail.com>
2025-08-19 23:07:17 -0300

    feature: adicionar funcionalidade de exibir pagina de receitas.

--- alura/vue/cookin-up/src/components/ConteudoPrincipal.vue
+++ alura/vue/cookin-up/src/components/ConteudoPrincipal.vue
@@ -1,14 +1,18 @@
 <script lang="ts">
+import MostrarReceitas from "./MostrarReceitas.vue";
 import SelecionarIngredientes from "./SelecionarIngredientes.vue";
 import SuaLista from "./SuaLista.vue";
+
+type Pagina = "SelecionarIngredientes" | "MostrarReceitas";
 
 export default {
   data() {
     return {
       ingredientes: [] as string[],
+      conteudo: "SelecionarIngredientes" as Pagina,
     };
   },
-  components: { SuaLista, SelecionarIngredientes },
+  components: { SuaLista, SelecionarIngredientes, MostrarReceitas },
   methods: {
     adicionarIngrediente(ingrediente: string) {
       this.ingredientes.push(ingrediente);
@@ -18,6 +22,9 @@ export default {
         (iLista) => ingrediente !== iLista
       );
     },
+    navegar(pagina: Pagina) {
+      this.conteudo = pagina;
+    },
   },
 };
 </script>
@@ -26,9 +33,12 @@ export default {
   <main class="conteudo-principal">
     <SuaLista :ingredientes="ingredientes" />
     <SelecionarIngredientes
+      v-if="conteudo === 'SelecionarIngredientes'"
       @adicionar-ingrediente="adicionarIngrediente"
       @remover-ingrediente="removerIngrediente"
+      @buscar-receitas="navegar('MostrarReceitas')"
     />
+    <MostrarReceitas v-else-if="conteudo === 'MostrarReceitas'" />
   </main>
 </template>
 
