ce173add5cc5b7b38ad84949da07cd864a01e1e5
Cayque Santos <cayque_santos16@hotmail.com>
2025-08-25 22:33:52 -0300

    refactor: alterado o Formulario para composition API.

--- alura/vue/alura-tracker/src/views/projetos/Formulario.vue
+++ alura/vue/alura-tracker/src/views/projetos/Formulario.vue
@@ -22,7 +22,7 @@ import useNotificador from "@/hooks/notificador";
 import { TipoNotificacao } from "@/interfaces/INotificacao";
 import { useStore } from "@/store";
 import { ALTERAR_PROJETO, CADASTRAR_PROJETO } from "@/store/tipo-acoes";
-import { defineComponent } from "vue";
+import { defineComponent, ref } from "vue";
 
 export default defineComponent({
   name: "Formulario",
@@ -30,19 +30,6 @@ export default defineComponent({
     id: {
       type: String,
     },
-  },
-  mounted() {
-    if (this.id) {
-      const projeto = this.store.state.projeto.projetos.find(
-        (proj) => proj.id == this.id
-      );
-      this.nomeDoProjeto = projeto?.nome || "";
-    }
-  },
-  data() {
-    return {
-      nomeDoProjeto: "",
-    };
   },
   methods: {
     salvar() {
@@ -69,12 +56,23 @@ export default defineComponent({
       this.$router.push("/projetos");
     },
   },
-  setup() {
+  setup(props) {
     const store = useStore();
     const { notificar } = useNotificador();
+
+    const nomeDoProjeto = ref("");
+
+    if (props.id) {
+      const projeto = store.state.projeto.projetos.find(
+        (proj) => proj.id == props.id
+      );
+      nomeDoProjeto.value = projeto?.nome || "";
+    }
+
     return {
       store,
       notificar,
+      nomeDoProjeto,
     };
   },
 });
