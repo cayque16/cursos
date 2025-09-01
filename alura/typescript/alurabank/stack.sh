db93bc7b815efb0b09f08f0fdd89b0dc1b240ab6
Cayque Santos <cayque_santos16@hotmail.com>
2025-09-01 17:12:14 -0300

    chore: define a versao do node usada.

--- alura/typescript/alurabank/stack.sh
+++ alura/typescript/alurabank/stack.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-IMAGE="node:latest"
+IMAGE="node:24.7.0"
 WORKDIR="/usr/src/myapp"
 # -u "node"
 docker run --rm --name node-teste -ti -p "3000:3000" -v "$PWD":"$WORKDIR" -w "$WORKDIR" "$IMAGE" "${@}"
