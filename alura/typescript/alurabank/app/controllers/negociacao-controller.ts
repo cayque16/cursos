03bb8a117e951e0e7079ad31f2b60187132d1199
Cayque Santos <cayque_santos16@hotmail.com>
2025-09-01 17:18:32 -0300

    feature: adiciona validacao para garantir que apenas negociacoes em dias uteis devem ser aceitas.

--- alura/typescript/alurabank/app/controllers/negociacao-controller.ts
+++ alura/typescript/alurabank/app/controllers/negociacao-controller.ts
@@ -1,3 +1,4 @@
+import { DiasDaSemana } from "../enums/dias-da-semana.js";
 import { Negociacao } from "../models/negociacao.js";
 import { Neogiciacoes } from "../models/negociacoes.js";
 import { MensagemView } from "../views/mensagem-view.js";
@@ -20,9 +21,24 @@ export class NegociacaoController {
 
   public adiciona(): void {
     const negociacao = this.criaNegociacao();
+
+    if (!this.ehDiaUtil(negociacao.data)) {
+      this.mensagemView.update(
+        "Apenas negociações em dias úteis podem ser adicionadas!!!"
+      );
+      return;
+    }
+
     this.negociacoes.adiciona(negociacao);
     this.limparFormulario();
     this.atualizaView();
+  }
+
+  private ehDiaUtil(data: Date): boolean {
+    return (
+      data.getDay() > DiasDaSemana.DOMINGO &&
+      data.getDay() < DiasDaSemana.SABADO
+    );
   }
 
   private criaNegociacao(): Negociacao {
