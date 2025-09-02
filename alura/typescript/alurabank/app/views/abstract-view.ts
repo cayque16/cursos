77646056a11d0cbee84bb67d14e04e3fa5b4546c
Cayque Santos <cayque_santos16@hotmail.com>
2025-09-01 22:31:17 -0300

    refactor: adiciona o strictNullChecks no tsconfig.

--- alura/typescript/alurabank/app/views/abstract-view.ts
+++ alura/typescript/alurabank/app/views/abstract-view.ts
@@ -3,7 +3,12 @@ export abstract class AbstactView<T> {
   private escapar = false;
 
   constructor(seletor: string, escapar?: boolean) {
-    this.elemento = document.querySelector(seletor);
+    const elemento = document.querySelector(seletor);
+    if (elemento) {
+      this.elemento = elemento as HTMLElement;
+    } else {
+      throw Error(`Seletor ${seletor} não existe no DOM. Favor verificar.`);
+    }
     if (escapar) {
       this.escapar = escapar;
     }
