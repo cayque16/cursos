962757a4d2307d66bde9952a0211bb290c660bd9
Cayque Santos <cayque_santos16@hotmail.com>
2025-09-01 23:14:31 -0300

    feature: adiciona um decorator para calcular o tempo de execucao dos metodos.

--- alura/typescript/alurabank/app/views/abstract-view.ts
+++ alura/typescript/alurabank/app/views/abstract-view.ts
@@ -1,3 +1,5 @@
+import { logarTempoDeExecucao } from "../decorators/logar-tempo-de-execucao.js";
+
 export abstract class AbstactView<T> {
   protected elemento: HTMLElement;
   private escapar = false;
@@ -14,6 +16,7 @@ export abstract class AbstactView<T> {
     }
   }
 
+  @logarTempoDeExecucao()
   public update(model: T): void {
     let template = this.template(model);
     if (this.escapar) {
